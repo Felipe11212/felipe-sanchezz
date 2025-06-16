6f4be5911a43e9df3a8533a204858f6c29fcecec
Felipe11212 <felipesanchezf816@gmail.com>
2025-06-16 08:14:28 -0500

    Update principal.py

--- crud/principal.py
+++ crud/principal.py
@@ -1,36 +1,32 @@
-from crear import crearPersona
-from eliminar import eliminar
-from actualizar import actualizar
-from mostrar import Mostrar
+from crear import crear_producto
+from mostrar import mostrar_productos
+from eliminar import eliminar_producto
+from actualizar import actualizar_producto
 
-personas = []
 def menu():
     while True:
-
-        print("1. Crear")
-        print("2. mostrar")
-        print("3. Actualizar ")
-        print("4. Eliminar")
+       
+        print("1. Crear producto")
+        print("2. Mostrar productos")
+        print("3. Actualizar producto")
+        print("4. Eliminar producto")
         print("5. Salir")
-        opcion = input("Selecciona una opcion: ")
+
+        opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
-            crearPersona()
-            
+            crear_producto()
         elif opcion == "2":
-            Mostrar()
-            
+            mostrar_productos()
         elif opcion == "3":
-            actualizar()
-            
+            actualizar_producto()
         elif opcion == "4":
-            eliminar()
-            
+            eliminar_producto()
         elif opcion == "5":
-            print("chao")
+            print("¡Hasta luego!")
             break
         else:
-            print("paila")
+            print("Opción no válida.")
 
-
-menu()
+if __name__ == "__main__":
+    menu()
