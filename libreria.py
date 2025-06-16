eea13414480602b53a1a9f1a8bf508c2e593a39f
Felipe11212 <felipesanchezf816@gmail.com>
2025-06-16 08:23:03 -0500

    Update libreria.py

--- libreria.py
+++ libreria.py
@@ -1,40 +1,13 @@
-from rich import print
-from rich.console import Console
-from rich.table import Table
-from rich.progress import track
-from time import sleep
+from cryptography.fernet import Fernet
 
-# 1. Texto colorido y estilos
-print("[bold magenta]Bienvenido a la demo de Rich![/bold magenta]")
+key = Fernet.generate_key()
+fernet = Fernet(key)
 
-# 2. Tabla de ejemplo
+mensaje = "clave123"
+token = fernet.encrypt(mensaje.encode())
+print("Encriptado:", token)
 
-# crea la tabla por consola
-console = Console()
-tabla = Table(title="Tabla de Inventario")
-
-# crea los espacios de las columnas
-tabla.add_column("Producto", justify="left", style="cyan")
-tabla.add_column("Cantidad", justify="center", style="green")
-tabla.add_column("Precio", justify="right", style="yellow")
-
-# crea las filas de la tabla
-tabla.add_row("Tomate", "25", "$1.00")
-tabla.add_row("Lechuga", "10", "$0.80")
-tabla.add_row("Zanahoria", "18", "$0.50")
-
-# muestra el resultado 
-console.print(tabla)
-print()
-
-# 3 simula una barra de progreso
-print("[bold blue]Simulando carga con barra de progreso:[/bold blue]")
-for paso in track(range(10), description="Cargando..."):
-    sleep(0.3)
-
-# Texto colorido y estilos
-print("\n[bold green]listo[/bold green]")
-
-# la (/n) es para el salto de linea
+desencriptado = fernet.decrypt(token).decode()
+print("Original:", desencriptado)
 
 
